db1d0cf6ab8d0ea8f337f0a2fddcb3178c717bb7
Marshano <Marshano_05@msn.com>
2020-12-01 14:42:46 +0100

    Add files via upload

---
+++ FirstContract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.6.10 and less than 0.7.0
+pragma solidity ^0.5.12;
+
+contract HelloWorld {
+    string public greet = "Hello World!";
+  	string public name = "Rain";
+  	int public level = 1;
+}
