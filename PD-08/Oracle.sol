dfdc78f016dd3194e9dfcd7e5887998407c41033
Marshano <Marshano_05@msn.com>
2021-01-30 12:28:24 +0100

    Add files via upload

---
+++ PD-08/Oracle.sol
@@ -0,0 +1,37 @@
+pragma solidity >= 0.5.0 < 0.6.0;
+
+import "github.com/provable-things/ethereum-api/provableAPI.sol";
+
+contract DieselPrice is usingProvable {
+
+    uint public dieselPriceUSD;
+
+    event LogNewDieselPrice(string price);
+    event LogNewProvableQuery(string description);
+
+    constructor()
+        public
+    {
+        update(); // First check at contract creation...
+    }
+
+    function __callback(
+        bytes32 _myid,
+        string memory _result
+    )
+        public
+    {
+        require(msg.sender == provable_cbAddress());
+        emit LogNewDieselPrice(_result);
+        dieselPriceUSD = parseInt(_result, 2); // Let's save it as cents...
+        // Now do something with the USD Diesel price...
+    }
+
+    function update()
+        public
+        payable
+    {
+        emit LogNewProvableQuery("Provable query was sent, standing by for the answer...");
+        provable_query("URL", "xml(https://www.fueleconomy.gov/ws/rest/fuelprices).fuelPrices.diesel");
+    }
+}
