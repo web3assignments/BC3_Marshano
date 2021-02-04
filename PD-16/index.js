3e99e24d699c7a84a6666e376186ecedbb188773
Marshano <Marshano_05@msn.com>
2021-02-04 18:03:28 +0100

    Add files via upload

---
+++ PD-16/index.js
@@ -0,0 +1,206 @@
+const contract_address = '0x85F2bb8d2EcaBbF3C163C56023A60193bd5376A9';
+const abi = [
+	{
+		"inputs": [],
+		"name": "deposit",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "payable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "balance",
+				"type": "uint256"
+			}
+		],
+		"name": "Deposit",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_text",
+				"type": "string"
+			}
+		],
+		"name": "depositText",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address payable",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "balance",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "balance",
+				"type": "uint256"
+			}
+		],
+		"name": "Withdraw",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "getBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "ownerContract",
+		"outputs": [
+			{
+				"internalType": "address payable",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "withdrawText",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+window.addEventListener('load', asyncloaded);    
+// Function from web3examples. responsible for loading the provider.
+async function asyncloaded() {
+    web3 = new Web3(Web3.givenProvider); // provider from metamask      
+    var result = await web3.eth.requestAccounts().catch(x => console.log(x.message));
+    console.log(`web3 is present: ${web3.version}`); // note: use ` (back quote)
+    const network = await web3.eth.net.getId().catch((reason) => console.log(`Cannnot find network ${reason}`));
+    if (typeof network === 'undefined' || network != 4) { console.log("Please select Rinkeby test network"); return; }
+    console.log("Ethereum network: Rinkeby")
+    accounts = await web3.eth.getAccounts();
+    console.log(accounts[0]); // show current user.
+    contract = new web3.eth.Contract(abi, contract_address);
+}
+window.addEventListener('load', asyncloaded);     
+
+//functie op data te verzenden
+function depositText(){
+    var address = document.getElementById("address").value;
+    console.log(address);
+    contract.methods.depositText(address).send({from: accounts[0]}).then(x => console.log(x));
+}
+//functie om data op te halen
+function withdrawText() {
+    //contract call naar de sol contract
+    contract.methods.withdrawText().call().then( function( text ) {
+      console.log("info: ", text);
+      document.getElementById('text').innerHTML = text;
+    });
+  }
+
+//functie om data op te halen
+function getBalance() {
+    //contract call naar de sol contract
+    contract.methods.getBalance().call().then( function( info ) {
+      console.log("info: ", info);
+      document.getElementById('lastInfo').innerHTML = info;
+    });
+  }
